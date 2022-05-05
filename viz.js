193039d39169dc7a0d0a2b056d154175a896bbc3
Mohammed Banani | MbDev <55842446+mbdev3@users.noreply.github.com>
2022-05-05 14:56:29 +0000

    Add files via upload

---
+++ viz.js
@@ -0,0 +1,37 @@
+import vl from "vega-lite-api";
+// export const viz = vl
+//   .markPoint({
+//     fill: true,
+//     stroke: false,
+//     size: 900,
+//     opacity: 0.1
+//   })
+//   .encode(
+//     vl.x().fieldQ('acceleration').scale({zero:false}),
+//     vl.y().fieldQ('horsepower').scale({zero:false}),
+//     vl.tooltip().fieldN('name')
+//   );
+// export const viz = vl
+//   .markCircle({
+//     size: 700,
+//     opacity: 0.5
+//   })
+//   .encode(
+//     vl.x().fieldQ('acceleration').scale({zero:false}),
+//     vl.y().fieldQ('horsepower').scale({zero:false}),
+//     vl.color().fieldN("origin"),
+//     vl.size().fieldQ("weight").scale({zero:false}),
+//     vl.tooltip().fieldN('name')
+//   );
+export const viz = vl
+  .markCircle({
+    size: 10000,
+    opacity: 0.2,
+  })
+  .encode(
+    vl.x().fieldN("origin"),
+    vl.y().fieldQ("horsepower").scale({ zero: false }),
+    vl.color().fieldN("origin"),
+    vl.size().fieldQ("horsepower").scale({ zero: false }),
+    vl.tooltip().fieldN("name")
+  );
